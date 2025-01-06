5d40db547acf095a477b78c6413c12300f3f84c0
Dave Goel <davegoel29@gmail.com>
2025-01-05 18:05:33 -0700

    fixed bug

--- cogweb/src/App.jsx
+++ cogweb/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import About from './Components/About/About'
-import Contact from './Components/Contact/contact'
+import Contact from './Components/Contact/Contact'
 import IB from './Components/IB/IB'
 import IB_desc from './Components/IB_desc/IB_desc'
 import NavBar from './Components/NavBar/NavBar'
